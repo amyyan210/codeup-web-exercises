2c8bf0cbc6b7d0bd925aa44779949e7abb23d1dd
amyyan210 <amyyan210@gmail.com>
2017-08-14 16:52:17 -0500

    Finish exercise for inline and external js
    * Add method to capitalize first letter of input, if not so already

--- js/external.js
+++ js/external.js
@@ -1,4 +1,4 @@
 console.log("Hello from external JavaScript");
 alert("Welcome to my website!");
 var faveColor = prompt("What is your favorite color?");
-alert("Cool! " + faveColor + " is my favorite color too!");
+alert("Cool! " + faveColor.charAt(0).toUpperCase() + faveColor.slice(1) + " is my favorite color too!");
