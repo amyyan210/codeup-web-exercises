9afb08b43fdfe8dec97633a0dd54788d6989d777
amyyan210 <amyyan210@gmail.com>
2017-08-14 16:43:52 -0500

    Finish exercise for inline and external js

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log("Hello from external JavaScript");
+alert("Welcome to my website!");
+var faveColor = prompt("What is your favorite color?");
+alert("Cool! " + faveColor + " is my favorite color too!");
