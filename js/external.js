a1dbfc59e28e0887ec8e74f10c40d2736baf8fc8
amyyan210 <amyyan210@gmail.com>
2017-08-15 11:09:58 -0500

    Finish exercise for if/else statements
    * Add console.log statements for each color of the rainbow array
    * Add ternary operator when fave color is equal to the random color

--- js/external.js
+++ js/external.js
@@ -1,6 +1,9 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
 alert("Welcome to my website!");
+
 var faveColor = prompt("What is your favorite color?");
+
 alert("Cool! " + faveColor.charAt(0).toUpperCase() + faveColor.slice(1) + " is my favorite color too!");
