9acb240ddefb9c15da182b157fe57e27df8442ba
amyyan210 <amyyan210@gmail.com>
2017-08-16 16:04:48 -0500

    Start for loop exercises
    * Write JS to create multiplication tables

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,2 +1,6 @@
 "use strict";
 
+var userNumber = prompt("Please enter a number between 1 and 10.");
+    for (var i = 1; i <= 10; i++) {
+        console.log(userNumber + " x " + i + " = " + (userNumber * i));
+    }
