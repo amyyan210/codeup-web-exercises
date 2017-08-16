7e480a5ed84c79e8201f33a6b5c1be6bd64d01db
amyyan210 <amyyan210@gmail.com>
2017-08-16 16:28:23 -0500

    Start for loop exercises
    * Write JS to generate 10 random numbers between 20 and 200 and console log whether each is even or odd

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,19 @@
 "use strict";
 
-var userNumber = prompt("Please enter a number between 1 and 10.");
+// var userNumber = prompt("Please enter a number between 1 and 10.");
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(userNumber + " x " + i + " = " + (userNumber * i));
+//     }
+
+// -----------------------------------------------------------------------------
+
+// This is how you get a random number between 20 and 200
     for (var i = 1; i <= 10; i++) {
-        console.log(userNumber + " x " + i + " = " + (userNumber * i));
+        var randomNumber = Math.floor(Math.random() * 180) + 20;
+
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + " is even.");
+        } else {
+            console.log(randomNumber + " is odd.");
+        }
     }
