2de04b7405d7da9a9c760a4305453ad5de7e8054
amyyan210 <amyyan210@gmail.com>
2017-08-16 15:47:55 -0500

    Start and finish while loop exercises

---
+++ js/while.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// var i = 2;
+//
+// while (i < 65537) {
+//     console.log(i);
+//     i = i * 2;
+// }
+
+//-------------------------------------------------------------------------------
+
+// This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//     console.log("I have to sell " + allCones + " cones in order to go home.");
+//
+// do {
+//     // This is how you get a random number between 1 and 5
+//     var cones = Math.floor(Math.random() * 5) + 1;
+//
+//     if (allCones >= cones) {
+//         console.log(cones + " cone(s) sold...");
+//         allCones = allCones - cones;
+//         console.log("The remaining amount of cones is " + allCones + ".");
+//     } else {
+//         console.log("Cannot sell you " + cones + " cone(s). I only have " + allCones + " cone(s) left.");
+//     }
+//
+//     if (allCones === 0) {
+//         console.log("Yay! I sold them all!");
+//         break;
+//     }
+//
+// } while (allCones > 0);
+
+//-------------------------------------------------------------------------------
+
+
+
+
